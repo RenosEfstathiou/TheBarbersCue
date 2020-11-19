f5ab16a615b179b729e15c11c4da37e21b5f20d0
Renos Efstathiou <it164826@it.teithe.gr>
2020-11-19 23:02:14 +0200

    Start Over

--- Routes/error.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (error,req,res,next) =>{
-  const statusCode = res.statusCode == 404 ? 404 : 500;
-  res.status(statusCode)
-  res.json({
-    message:error.message,
-  })
-}
-module.exports = errorHandler;
--- database/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb+srv://User1:${process.env.MONGO_PASSWORD}@cluster0-n88gy.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-{useNewUrlParser: true,useUnifiedTopology:true},
-()=> {
-    console.log('connected to db ');
-});
-
